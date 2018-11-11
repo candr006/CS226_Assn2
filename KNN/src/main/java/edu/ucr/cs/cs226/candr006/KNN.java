44dd58b03e688c5a9119f2639feb84b259eb4128
candr006 <candr006@ucr.edu>
2018-11-11 03:17:29 +0000

    initial KNN file setup

---
+++ KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
@@ -0,0 +1,13 @@
+package edu.ucr.cs.cs226.candr006;
+
+/**
+ * Hello world!
+ *
+ */
+public class KNN 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
